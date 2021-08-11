2006190fbdbcf84b003e0653210f895fa82d413d
Robert Booth <boothr16@gmail.com>
2021-08-11 13:03:01 -0400

    Added clarifying comments to script.js

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-const currencyElementOne = document.getElementById('currency-one');
-const amountElementOne = document.getElementById('amount-one');
-const currencyElementTwo = document.getElementById('currency-two');
-const amountElementTwo = document.getElementById('amount-two');
+const currencyElementOne = document.getElementById('currency-one'); // original currency choice
+const amountElementOne = document.getElementById('amount-one'); // user-input amount
+const currencyElementTwo = document.getElementById('currency-two'); // target currency choice
+const amountElementTwo = document.getElementById('amount-two'); // calculated amount dependent on conv. rate
 
-const rateElement = document.getElementById('rate');
-const swap = document.getElementById('swap');
+const rateElement = document.getElementById('rate'); // shows conversion rate between the two currencies
+const swap = document.getElementById('swap'); // swap button
 
 // Fetch exchange rates and update the DOM
 function calculate() {
-    const currency_one = currencyElementOne.value;
-    const currency_two = currencyElementTwo.value;
+    const currency_one = currencyElementOne.value; // USD, EUR, etc.
+    const currency_two = currencyElementTwo.value; // USD, EUR, etc.
 
     fetch(`https://v6.exchangerate-api.com/v6/d8bb1361a95661af8cc9a984/latest/${currency_one}`)
-    .then(res => res.json())
+    .then(res => res.json()) // res.json() = response from website in JSON format
     .then(data => {
         // console.log(data)
-        const rate = data.conversion_rates[currency_two];
+        const rate = data.conversion_rates[currency_two]; // conversion_rates is an array from API
+        // [currency_two] looks up numerical value at index currency_two
 
-        rateElement.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+        rateElement.innerText = `1 ${currency_one} = ${rate} ${currency_two}`; // updates conv. rate on screen
 
-        amountElementTwo.value = (amountElementOne.value * rate).toFixed(2);
+        amountElementTwo.value = (amountElementOne.value * rate).toFixed(2); // to at least 2 decimal places
     });
 }
 
@@ -30,6 +31,7 @@ currencyElementTwo.addEventListener('change', calculate);
 amountElementTwo.addEventListener('input', calculate);
 
 swap.addEventListener('click', () => {
+    // perform generic swap algorithm on click, and then calculate new conv. rate and display
     const temp = currencyElementOne.value;
     currencyElementOne.value = currencyElementTwo.value;
     currencyElementTwo.value = temp;
